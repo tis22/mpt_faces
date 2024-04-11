bcddb38463f0ca1d5b8aac3507a4ff16c8c6c269
Tim S. <timsan99@gmx.de>
2024-04-11 15:05:27 +0200

    Haar feature cascade, saving the images and coordinates

--- record.py
+++ record.py
@@ -5,7 +5,7 @@ import gdown
 import uuid
 import csv
 from common import ROOT_FOLDER
-from cascade import create_cascade
+#from cascade import create_cascade
 
 # Quellen
 #  - How to open the webcam: https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
@@ -28,13 +28,61 @@ def record(args):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
-
     #   Open The OpenCV VideoCapture Device to retrieve live images from your webcam (cv.VideoCapture)
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    frame_counter = 0
+    saving_blocker = False
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
     #   Initialize the Haar feature cascade for face recognition from OpenCV (cv.CascadeClassifier)
+        face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    
     #   If the cascade file (haarcascade_frontalface_default.xml) is missing, download it from google drive
+    #   ??
+
     #   Run the cascade on every image to detect possible faces (CascadeClassifier::detectMultiScale)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+            frame = cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+
+        cv.imshow('webcam',frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+        
+
     #   If there is exactly one face, write the image and the face position to disk in two seperate files (cv.imwrite, csv.writer)
     #   If you have just saved, block saving for 30 consecutive frames to make sure you get good variance of images.
+        if len(faces) == 1 and not saving_blocker:
+            filename = str(uuid.uuid4())
+            file_path = os.path.join(folder_path, filename)
+
+            cv.imwrite(file_path + '.jpg',frame)
+
+            csvfile = open(file_path + '.csv', 'w', newline='', encoding='utf-8')
+            c = csv.writer(csvfile)
+            for (x, y, w, h) in faces:
+                c.writerow([x, y, w, h])
+            csvfile.close()
+            saving_blocker = True
+
+        else:
+            frame_counter += 1
+            if frame_counter >= 30:
+                frame_counter = 0
+                saving_blocker = False
+            
+    
     if args.folder is None:
         print("Please specify folder for data to be recorded into")
         exit()
