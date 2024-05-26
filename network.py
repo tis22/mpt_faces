dded513a1ae5ae06bf14f360a2ac057e362cb377
eighthill <meric-2000@web.de>
2024-05-26 20:38:06 +0200

    cleaned up network.py

--- network.py
+++ network.py
@@ -13,15 +13,13 @@ class Net(nn.Module):
         # Input imags will be 3 channels 256x256 pixels.
         # Output must be a nClasses Tensor.
 
-        # First convolutional layer
+        # Convolutional layers
         self.conv1 = nn.Conv2d(3, 32, kernel_size=5, stride=1, padding=2)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-
-        # Second convolutional layer
         self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2)
-
-        # Third convolutional layer
         self.conv3 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2)
+
+        # Max-pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         # Fully connected layers
         self.fc1 = nn.Linear(128 * 32 * 32, 512)
@@ -33,16 +31,12 @@ class Net(nn.Module):
         #  x is a BATCH_SIZEx3x256x256 Tensor
         #  return value must be a BATCH_SIZExN_CLASSES Tensor
 
-        # conv1 + ReLU + MaxPooling
+        # conv layers ReLU activation and MaxPooling
         x = self.pool(F.relu(self.conv1(x)))
-
-        # conv2 + ReLU + MaxPooling
         x = self.pool(F.relu(self.conv2(x)))
-
-        # conv3 + ReLU + MaxPooling
         x = self.pool(F.relu(self.conv3(x)))
 
-        # Flatten output for fully connected layer
+        # Flattened output for fully connected layer
         x = x.view(-1, 128 * 32 * 32)
 
         # Fully connected layer, ReLU activated
